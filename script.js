6c2cc778b82403abc940b64e0224e3ad0087de07
Purcaru Nicolae <77527495+PurcaruNicolae@users.noreply.github.com>
2021-07-05 18:46:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('.input')
+let button = document.querySelector('.btn')
+let search = document.querySelector('.search')
+
+button.addEventListener('click', () => {
+    search.classList.toggle('move')
+    input: focus()
+
+});
